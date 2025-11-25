a4d42154bacec054430310fee38273e7680ad38a
myasnoeee-droid <myasnoeee@gmail.com>
2025-11-25 17:10:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -136,8 +136,7 @@ def lang_choice_keyboard() -> InlineKeyboardMarkup:
         inline_keyboard=[
             [InlineKeyboardButton(text=t1, callback_data="mode:animate")],
             [InlineKeyboardButton(text=t2, callback_data="mode:lipsync")],
-        ]
-        
+        ]    
     )
 
 # ---------- Пресеты (региональные) ----------
